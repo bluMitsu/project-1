1606419e9071412b4e393e2b239be5a76790d507
bluMitsu <42302276+bluMitsu@users.noreply.github.com>
2018-08-17 22:27:00 +0200

    Update main Code.py

--- main Code.py
+++ main Code.py
@@ -26,8 +26,6 @@ def create_data_structure(string_input):
         string_input= string_input[string_input.find('.', first_dot+1 ) + 1:]
     return network
 
-print create_data_structure(example_input)
-
 def get_connections(network, user):
     if user in network:    
         return network[user]['connections']
@@ -94,3 +92,41 @@ def friends (network, user_A, user_B, friend = None):
             if next_friend:
                 return next_friend 
     return None
+
+#Test example:
+
+example_input="John is connected to Bryant, Debra, Walter.\
+John likes to play The Movie: The Game, The Legend of Corgi, Dinosaur Diner.\
+Bryant is connected to Olive, Ollie, Freda, Mercedes.\
+Bryant likes to play City Comptroller: The Fiscal Dilemma, Super Mushroom Man.\
+Mercedes is connected to Walter, Robin, Bryant.\
+Mercedes likes to play The Legend of Corgi, Pirates in Java Island, Seahorse Adventures.\
+Olive is connected to John, Ollie.\
+Olive likes to play The Legend of Corgi, Starfleet Commander.\
+Debra is connected to Walter, Levi, Jennie, Robin.\
+Debra likes to play Seven Schemers, Pirates in Java Island, Dwarves and Swords.\
+Walter is connected to John, Levi, Bryant.\
+Walter likes to play Seahorse Adventures, Ninja Hamsters, Super Mushroom Man.\
+Levi is connected to Ollie, John, Walter.\
+Levi likes to play The Legend of Corgi, Seven Schemers, City Comptroller: The Fiscal Dilemma.\
+Ollie is connected to Mercedes, Freda, Bryant.\
+Ollie likes to play Call of Arms, Dwarves and Swords, The Movie: The Game.\
+Jennie is connected to Levi, John, Freda, Robin.\
+Jennie likes to play Super Mushroom Man, Dinosaur Diner, Call of Arms.\
+Robin is connected to Ollie.\
+Robin likes to play Call of Arms, Dwarves and Swords.\
+Freda is connected to Olive, John, Debra.\
+Freda likes to play Starfleet Commander, Ninja Hamsters, Seahorse Adventures."
+
+
+net = create_data_structure(example_input)
+
+print net
+print get_connections(net, "Debra")
+print get_connections(net, "Mercedes")
+print get_games_liked(net, "John")
+print add_connection(net, "John", "Freda")
+print add_new_user(net, "Debra", [])
+print get_secondary_connections(net, "Mercedes")
+print count_common_connections(net, "Ollie", "John")
+print find_path_to_friend(net, "John","Ollie")
